9ff393121ab83066b3fa22d7455bf900265f0030
mapez <martamalopez@gmail.com>
2014-11-21 12:17:25 +0100

    Neto

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,9 +1,15 @@
 class MovementsController < ApplicationController
 
   def index
-
+    @sum = 0
     @incomes = current_user.incomes.order(id: :desc).limit(5)
     @expenses = current_user.expenses.order(id: :desc).limit(5)
+    @incomes.each do |inc|
+      @sum += inc.amount
+    end
+    @expenses.each do |inc|
+      @sum += inc.amount
+    end
   end
 
   
